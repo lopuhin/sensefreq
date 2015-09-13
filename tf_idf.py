3f5b88f684a632d359cae44723dc97e262d208db
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2015-09-13 11:49:14 +0300

    output final weight by default

--- tf_idf.py
+++ tf_idf.py
@@ -5,12 +5,13 @@ from __future__ import division
 
 import sys
 import codecs
+import math
 from collections import defaultdict
 
 from utils import w2v_counts, w2v_total_count
 
 
-def main(filename):
+def main(filename, analyse=False):
     total_count = w2v_total_count()
     ff = lambda x: '%.5f' % x
     with codecs.open(filename, 'rb', 'utf-8') as f:
@@ -30,10 +31,15 @@ def main(filename):
         for w, c, gc in sorted(
                 counts, key=lambda (_, c, gc): c, reverse=True):
             pred_count = gc * (contexts_count / total_count)
-            print ''.join(str(x).ljust(20) for x in [
-                w.ljust(50).encode('utf-8'),
-                c, gc, ff(pred_count), ff(c / pred_count)]).rstrip()
-            # ln(c / pred_count) looks like a good measure
+            if analyse:
+                print ''.join(str(x).ljust(20) for x in [
+                    w.ljust(50).encode('utf-8'),
+                    c, gc, ff(pred_count), ff(c / pred_count)]).rstrip()
+                # ln(c / pred_count) looks like a good measure
+            else:
+                # just output final weight
+                weight = max(0, math.log(c / pred_count))
+                print w.encode('utf-8'), weight
 
 
 if __name__ == '__main__':
