565a35adef6e92051c5d62cdd6bb2ae6350934af
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2015-10-11 13:04:01 +0300

    extract words from a large word list

--- extract_contexts.py
+++ extract_contexts.py
@@ -1,33 +1,47 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-import sys
+import os
 import itertools
+import argparse
+import codecs
 
 
-def main(corpus, word):
-    word = word.decode('utf-8')
-    for ctx in contexts_iter(corpus, word):
-        print ' '.join(ctx).encode('utf-8')
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('corpus')
+    parser.add_argument('output')
+    parser.add_argument('--words', help='comma-separated')
+    parser.add_argument('--wordlist', help='one word on a line')
+    args = parser.parse_args()
+    if args.words:
+        words = args.words.decode('utf-8').split(',')
+    else:
+        with codecs.open(args.wordlist, 'rb', 'utf-8') as f:
+            words = [line.strip() for line in f]
+    files = {
+        w: codecs.open(os.path.join(args.output, w + '.txt'), 'wb', 'utf-8')
+        for w in words}
+    for before, w, after in contexts_iter(args.corpus, words):
+        files[w].write(u'\t'.join([before, w, after]) + u'\n')
+    for f in files.itervalues():
+        f.close()
 
 
-def contexts_iter(corpus, word, window_size=8):
+def contexts_iter(corpus, words, window_size=12):
     with open(corpus, 'rb') as f:
-        words = (w for line in f for w in line.decode('utf-8').split())
+        corpus_iter = (w for line in f for w in line.decode('utf-8').split())
         while True:
             # do not care if we miss some context with low prob.
-            chunk = list(itertools.islice(words, 100000))
+            chunk = list(itertools.islice(corpus_iter, 100000))
             if not chunk:
                 break
             for idx, w in enumerate(chunk):
-                if w == word:
-                    yield chunk[max(0, idx - window_size) :
-                                idx + window_size + 1]
+                if w in words:
+                    before = u' '.join(chunk[max(0, idx - window_size) : idx])
+                    after = u' '.join(chunk[idx + 1 : idx + window_size + 1])
+                    yield before, w, after
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        main(*sys.argv[1:])
-    else:
-        print 'usage: ./extract_contexts.py corpus word'
-        sys.exit(-1)
+    main()
