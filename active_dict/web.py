c69e66bd5e896b9ab7cd9d2c6fe1e9bca2462da7
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2015-10-18 12:53:44 +0300

    render list of words

--- active_dict/web.py
+++ active_dict/web.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+import json
+import os.path
 import argparse
 
 import tornado.ioloop
 import tornado.web
 
 
-class WordsHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def ctx_root(self):
+        return self.application.settings['ctx_root']
+
+    @property
+    def ad_root(self):
+        return os.path.dirname(self.ctx_root)
+
+
+class WordsHandler(BaseHandler):
     def get(self):
-        self.render('templates/words.html')
+        with open(os.path.join(self.ctx_root, 'summary.json'), 'rb') as f:
+            summary = json.load(f)
+        words = sorted(
+            (word, len(freqs), sorted(freqs.itervalues(), reverse=True))
+            for word, freqs in summary.iteritems())
+        self.render(
+            'templates/words.html',
+            words=words,
+            )
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('ad_root')
+    parser.add_argument('ctx_root')
     parser.add_argument('--port', type=int, default=8000)
     parser.add_argument('--debug', action='store_true')
     args = parser.parse_args()
@@ -22,7 +42,7 @@ def main():
         (r'/', WordsHandler),
         ],
         static_path='./active_dict/static',
-        debug=args.debug,
+        **vars(args)
     )
     application.listen(args.port)
     tornado.ioloop.IOLoop.current().start()
