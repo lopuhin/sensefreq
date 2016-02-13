b473cc984488ae705df1535dffde6de79cc9101b
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-02-13 22:57:13 +0300

    port build_weights to py3

--- tools/build_weights.py
+++ tools/build_weights.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-
-from __future__ import division
-
 import os
 import argparse
-import codecs
 import math
 from collections import defaultdict
 
@@ -34,7 +29,7 @@ def write_cdict(input_filename, output_filename):
     min_count = 4
     total_count = w2v_total_count()
     ff = lambda x: '%.5f' % x
-    with codecs.open(input_filename, 'rb', 'utf-8') as in_f:
+    with open(input_filename, 'r') as in_f:
         counts = defaultdict(int)
         seen = set()
         for line in in_f:
@@ -43,25 +38,25 @@ def write_cdict(input_filename, output_filename):
             seen.add(line)
             for w in line.strip().split():
                 counts[w] += 1
-        contexts_count = sum(counts.itervalues())
+        contexts_count = sum(counts.values())
         words = list(counts)
         global_counts = dict(zip(words, w2v_counts(words)))
-        counts = [(w, c, global_counts[w]) for w, c in counts.iteritems()
+        counts = [(w, c, global_counts[w]) for w, c in counts.items()
                   if c >= min_count and global_counts.get(w) is not None]
-        with open(output_filename, 'wb') as out_f:
+        with open(output_filename, 'w') as out_f:
             for w, c, gc in sorted(
-                    counts, key=lambda (_, c, gc): c, reverse=True):
+                    counts, key=lambda x: x[1], reverse=True):
                 pred_count = gc * (contexts_count / total_count)
                 if analyse:
                     out_f.write(''.join(str(x).ljust(20) for x in [
-                        w.ljust(50).encode('utf-8'),
+                        w.ljust(50),
                         c, gc, ff(pred_count), ff(c / pred_count)]).rstrip())
                     out_f.write('\n')
                     # ln(c / pred_count) looks like a good measure
                 else:
                     # just output final weight
                     weight = max(0, math.log(c / pred_count))
-                    out_f.write('%s %s\n' % (w.encode('utf-8'), weight))
+                    out_f.write('%s %s\n' % (w, weight))
 
 
 if __name__ == '__main__':
