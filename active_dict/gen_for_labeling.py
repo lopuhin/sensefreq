a73bbfade12e6d8347568eaac69a035fdc4b633e
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-02-15 20:42:45 +0300

    py3 compat in gen_for_labeling

--- active_dict/gen_for_labeling.py
+++ active_dict/gen_for_labeling.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-# encoding: utf-8
 
-from __future__ import division, print_function
+from __future__ import print_function
 import argparse
 import os.path
 import random
@@ -17,18 +16,17 @@ def main():
     parser.add_argument('--n', type=int, default=100)
     args = parser.parse_args()
 
-    word = args.word.decode('utf-8')
-    w_info = get_ad_word(word, args.ad_root, with_contexts=False)
-    with open(os.path.join(args.contexts_root, args.word + '.txt'), 'rb') as f:
+    w_info = get_ad_word(args.word, args.ad_root, with_contexts=False)
+    with open(os.path.join(args.contexts_root, args.word + '.txt'), 'r') as f:
         contexts = list(f)
     random.seed(1)
     random.shuffle(contexts)
     contexts = contexts[:args.n]
     for m in w_info['meanings'] + [
-            dict(name=u'Другое', id=len(w_info['meanings']) + 1),
-            dict(name=u'Не могу определить', id=0)]:
-        print(('\t\t%s: %s\t\t%s'
-            % (m['name'], m.get('meaning', ''), m['id'])).encode('utf-8'))
+            dict(name='Другое', id=len(w_info['meanings']) + 1),
+            dict(name='Не могу определить', id=0)]:
+        print(('\t\t%s: %s\t\t%s' % (
+            m['name'], m.get('meaning', ''), m['id'])))
     for ctx in contexts:
         print(ctx, end='')
 
