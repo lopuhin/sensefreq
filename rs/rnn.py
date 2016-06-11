058cb88e6462caff8f0e4747ad16c73e901a7c1b
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-06-11 10:28:57 +0300

    Feed left and right contexts

--- rs/rnn.py
+++ rs/rnn.py
@@ -5,7 +5,7 @@ import pickle
 import os.path
 
 from keras.models import Graph
-from keras.layers.core import Dense  # , Dropout
+from keras.layers.core import Dense, Dropout
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM
 from keras.utils.np_utils import to_categorical
@@ -41,47 +41,48 @@ def get_features(corpus: str, *, n_features: int) -> (int, [str]):
     return result
 
 
-def data_gen(corpus, *, words, n_features, max_len):
+def data_gen(corpus, *, words: [str], n_features: int, window: int):
     # PAD = 0
     UNK = 1
     words = words[:n_features - 2]  # for UNK and PAD
     idx_to_word = {word: idx for idx, word in enumerate(words, 2)}
+    to_arr = lambda input: np.array([
+        [idx_to_word.get(w, UNK) for w in input]], dtype=np.int32)
     while True:
         buffer = []
         for word in corpus_reader(corpus):
             buffer.append(word)
             # TODO - add random padding?
-            # TODO - pack more samples
-            if len(buffer) > max_len:
-                input = buffer[-max_len - 1 : -1]
-                output = buffer[-1]
-                one_hot = lambda x: to_categorical([x], nb_classes=n_features)
+            # TODO - pack more samples?
+            if len(buffer) > 2 * window:
+                left = buffer[-2 * window - 1 : -window - 1]
+                output = buffer[-window - 1]
+                right = buffer[-window:]
                 yield dict(
-                    input=np.array([
-                        [idx_to_word.get(w, UNK) for w in input]],
-                        dtype=np.int32),
-                    output=np.array([
-                        idx_to_word.get(output, UNK)], dtype=np.int32),
+                    left=to_arr(left),
+                    right=to_arr(right),
+                    output=to_arr([output])[:,0],
                 )
             if len(buffer) > 10000:
-                buffer[:-max_len] = []
+                buffer[: -2 * window] = []
 
 
-def build_model(*, n_features, embedding_size, hidden_size, max_len):
+def build_model(*, n_features, embedding_size, hidden_size, window):
     print('Building model...', end=' ', flush=True)
+    # TODO - use "non-legacy" way (?)
     model = Graph()
-    # TODO - use "non-legacy" way
-    model.add_input(name='input', input_shape=(max_len,), dtype='int')
-    model.add_node(Embedding(n_features, embedding_size, input_length=max_len),
-                   name='embedding', input='input')
-    # TODO - make forward and backward take different parts of inputs
-    model.add_node(LSTM(hidden_size), name='forward', input='embedding')
-    # model.add_node(LSTM(hidden_size, go_backwards=True),
-                     # name='backward', input='embedding')
-    # model.add_node(Dropout(0.5), name='dropout', inputs=['forward', 'backward'])
+    model.add_input(name='left', input_shape=(window,), dtype='int')
+    model.add_input(name='right', input_shape=(window,), dtype='int')
+    embedding = Embedding(
+        n_features, embedding_size, input_length=window, mask_zero=True)
+    model.add_node(embedding, name='left_embedding', input='left')
+    model.add_node(embedding, name='right_embedding', input='right')
+    model.add_node(LSTM(hidden_size), name='forward', input='left_embedding')
+    model.add_node(LSTM(hidden_size, go_backwards=True),
+                   name='backward', input='right_embedding')
+    model.add_node(Dropout(0.5), name='dropout', inputs=['forward', 'backward'])
     model.add_node(Dense(n_features, activation='softmax'),
-                   # name='softmax', input='dropout')
-                   name='softmax', input='forward')
+                   name='softmax', input='dropout')
     model.add_output(name='output', input='softmax')
     model.compile(loss='sparse_categorical_crossentropy', optimizer='rmsprop')
     print('done')
@@ -94,19 +95,19 @@ def main():
     parser.add_argument('--n-features', type=int, default=10000)
     parser.add_argument('--embedding-size', type=int, default=128)
     parser.add_argument('--hidden-size', type=int, default=64)
-    parser.add_argument('--max-len', type=int, default=10)
+    parser.add_argument('--window', type=int, default=10)
     args = parser.parse_args()
 
     model = build_model(
         n_features=args.n_features,
         embedding_size=args.embedding_size,
         hidden_size=args.hidden_size,
-        max_len=args.max_len)
+        window=args.window)
     n_tokens, words = get_features(args.corpus, n_features=args.n_features)
     model.fit_generator(
         generator=data_gen(
             args.corpus,
-            words=words, max_len=args.max_len, n_features=args.n_features),
+            words=words, window=args.window, n_features=args.n_features),
         samples_per_epoch=n_tokens,
         nb_epoch=10)
 
