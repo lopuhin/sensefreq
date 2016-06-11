d916d51eda72e4bffeab0226fa3af7a1a998e945
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-06-11 10:49:01 +0300

    Train in batches

--- rs/rnn.py
+++ rs/rnn.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 import argparse
-from collections import Counter
-import pickle
+from collections import Counter, defaultdict
 import os.path
+import pickle
 
 from keras.models import Graph
 from keras.layers.core import Dense, Dropout
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM
-from keras.utils.np_utils import to_categorical
 import numpy as np
 
 
 # FIXME - use real types
+List = Iterator = Dict = defaultdict(int)
+Any = None
 
 
-def corpus_reader(corpus: str) -> [str]:
+def corpus_reader(corpus: str) -> Iterator[str]:
     """ Iterate over words in corpus.
     """
     # assume lemmatized and tokenized corpus
@@ -25,7 +26,7 @@ def corpus_reader(corpus: str) -> [str]:
                 yield word
 
 
-def get_features(corpus: str, *, n_features: int) -> (int, [str]):
+def get_features(corpus: str, *, n_features: int) -> (int, List[str]):
     cached_filename = '{}.f{}.pkl'.format(corpus, n_features)
     if os.path.exists(cached_filename):
         with open(cached_filename, 'rb') as f:
@@ -41,33 +42,47 @@ def get_features(corpus: str, *, n_features: int) -> (int, [str]):
     return result
 
 
-def data_gen(corpus, *, words: [str], n_features: int, window: int):
+def data_gen(corpus, *, words: [str],
+             n_features: int, window: int, batch_size: int)\
+        -> Iterator[Dict[str, np.ndarray]]:
     # PAD = 0
     UNK = 1
     words = words[:n_features - 2]  # for UNK and PAD
     idx_to_word = {word: idx for idx, word in enumerate(words, 2)}
-    to_arr = lambda input: np.array([
-        [idx_to_word.get(w, UNK) for w in input]], dtype=np.int32)
+
+    def to_arr(contexts: List[(List[str], List[str], str)], idx: int)\
+            -> np.ndarray:
+        return np.array(
+            [[idx_to_word.get(w, UNK) for w in context[idx]]
+             for context in contexts],
+            dtype=np.int32)
+
+    buffer_max_size = 10000
     while True:
         buffer = []
+        batch = []
         for word in corpus_reader(corpus):
             buffer.append(word)
             # TODO - add random padding?
-            # TODO - pack more samples?
+            # TODO - some shuffling?
             if len(buffer) > 2 * window:
                 left = buffer[-2 * window - 1 : -window - 1]
-                output = buffer[-window - 1]
+                output = buffer[-window - 1 : -window]
                 right = buffer[-window:]
+                batch.append((left, right, output))
+            if len(batch) == batch_size:
                 yield dict(
-                    left=to_arr(left),
-                    right=to_arr(right),
-                    output=to_arr([output])[:,0],
+                    left=to_arr(batch, 0),
+                    right=to_arr(batch, 1),
+                    output=to_arr(batch, 2)[:,0],
                 )
-            if len(buffer) > 10000:
+                batch[:] = []
+            if len(buffer) > buffer_max_size:
                 buffer[: -2 * window] = []
 
 
-def build_model(*, n_features, embedding_size, hidden_size, window):
+def build_model(*, n_features: int, embedding_size: int, hidden_size: int,
+                window: int) -> Graph:
     print('Building model...', end=' ', flush=True)
     # TODO - use "non-legacy" way (?)
     model = Graph()
@@ -96,6 +111,8 @@ def main():
     parser.add_argument('--embedding-size', type=int, default=128)
     parser.add_argument('--hidden-size', type=int, default=64)
     parser.add_argument('--window', type=int, default=10)
+    parser.add_argument('--batch-size', type=int, default=16)
+    parser.add_argument('--n-epochs', type=int, default=1)
     args = parser.parse_args()
 
     model = build_model(
@@ -107,9 +124,12 @@ def main():
     model.fit_generator(
         generator=data_gen(
             args.corpus,
-            words=words, window=args.window, n_features=args.n_features),
+            words=words,
+            window=args.window,
+            n_features=args.n_features,
+            batch_size=args.batch_size),
         samples_per_epoch=n_tokens,
-        nb_epoch=10)
+        nb_epoch=args.n_epochs)
 
 
 if __name__ == '__main__':
