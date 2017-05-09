e35102be27641c37f1da221a48717d810da705f0
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-05-09 15:42:24 +0300

    Context extraction: add progress bar, little cleanup

--- rs/tools/extract_contexts.py
+++ rs/tools/extract_contexts.py
@@ -4,18 +4,20 @@ import itertools
 import argparse
 import gzip
 
+import tqdm
+
 from rlwsd.utils import normalize
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('corpus')
-    parser.add_argument('output')
-    parser.add_argument('--words', help='comma-separated')
-    parser.add_argument('--wordlist', help='one word on a line')
-    parser.add_argument('--lines', action='store_true',
-        help='context only on one line')
-    parser.add_argument('--window', type=int, default=12)
+    arg = parser.add_argument
+    arg('corpus')
+    arg('output')
+    arg('--words', help='comma-separated')
+    arg('--wordlist', help='one word on a line')
+    arg('--lines', action='store_true', help='context only on one line')
+    arg('--window', type=int, default=12)
     args = parser.parse_args()
     if args.words:
         words = args.words.split(',')
@@ -24,10 +26,10 @@ def main():
             words = [line.strip() for line in f]
     else:
         parser.error('Specify either --words or --wordlist')
-    files = {w: open(os.path.join(args.output, w + '.txt'), 'w') for w in words}
-    filenames = [
-        os.path.join(args.corpus, fname) for fname in os.listdir(args.corpus)]\
-        if os.path.isdir(args.corpus) else [args.corpus]
+    files = {w: open(os.path.join(args.output, w + '.txt'), 'wt') for w in words}
+    filenames = (
+        [os.path.join(args.corpus, fname) for fname in os.listdir(args.corpus)]
+        if os.path.isdir(args.corpus) else [args.corpus])
     fn = line_contexts_iter if args.lines else contexts_iter
     for fname in filenames:
         open_fn = gzip.open if fname.endswith('.gz') else open
@@ -43,9 +45,16 @@ def contexts_iter(f, words, window_size):
     canonical_words = {w: w for w in words}
     for w in words:
         canonical_words[w.replace('ё', 'е')] = w
+    file_size = os.path.getsize(f.name)
+    print('Total size: {:,} bytes'.format(file_size))
+    pbar = tqdm.tqdm(total=file_size)
+    pos = 0
     while True:
         # do not care if we miss some context with low prob.
         chunk = list(itertools.islice(corpus_iter, 100000))
+        new_pos = f.tell()
+        pbar.update(new_pos - pos)
+        pos = new_pos
         if not chunk:
             break
         join = lambda s, e: ' '.join(map(normalize, chunk[s:e]))
@@ -55,6 +64,7 @@ def contexts_iter(f, words, window_size):
                 before = join(max(0, idx - window_size), idx)
                 after = join(idx + 1, idx + window_size + 1)
                 yield before, canonical_w, after
+    pbar.close()
 
 
 def line_contexts_iter(f, words, window_size):
